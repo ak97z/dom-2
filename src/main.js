16ec2f58934d8cc1ce44a56d92b5b1eff4f09757
ak <123@qq.com>
2021-12-06 10:11:02 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
-jQuery(('#test'))
+
+const a =jQuery('.test')
+.addClass('red')
+.find('.child')
+.addClass('blue')
+.end()
+.addClass('green')
+// .each(function (x,y) {
+//     console.log(x,y)
+// })
+.find('.child')
+// .each((div)=>console.log(div))
+.end()
+.children().print() 
+
+
+console.log(a)//jQuery函数的结构 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
